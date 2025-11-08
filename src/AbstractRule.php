0a1b35fd4b2998fbc61a342c3e6fe599f9c82c9f
Takayuki Miyoshi <takayukister@gmail.com>
2025-11-08 18:19:15 +0900

    Edit right file

--- src/AbstractRule.php
+++ src/AbstractRule.php
@@ -2,5 +2,44 @@
 
 namespace RockLobsterInc\SchemaWovenValidation;
 
+/**
+ * Abstract class that represents the root of all SWV rules.
+ */
 abstract class AbstractRule {
+
+	/**
+	 * Properties of the rule.
+	 */
+	protected $properties = array();
+
+
+	/**
+	 * Constructor.
+	 *
+	 * @param array $properties Properties of the rule.
+	 */
+	public function __construct( array $properties = [] ) {
+		$this->properties = $properties;
+	}
+
+
+	/**
+	 * Returns true if this rule matches the given context.
+	 *
+	 * @param array $context Context.
+	 */
+	public function matches( array $context ) {
+		return true;
+	}
+
+
+	/**
+	 * Validates with this rule's logic.
+	 *
+	 * @param array $context Context.
+	 */
+	public function validate( array $context ) {
+		return true;
+	}
+
 }
