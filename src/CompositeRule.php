ed25272ee631280d62b6d2771a220b5aa8b1a6dc
Takayuki Miyoshi <takayukister@gmail.com>
2025-11-08 18:31:44 +0900

    Update CompositeRule.php

--- src/CompositeRule.php
+++ src/CompositeRule.php
@@ -2,5 +2,26 @@
 
 namespace RockLobsterInc\SchemaWovenValidation;
 
+/**
+ * Abstract class that represents the root of all composite-type SWV rules.
+ */
 abstract class CompositeRule extends AbstractRule {
+
+	/**
+	 * Sub-rules of the rule.
+	 */
+	protected $rules = array();
+
+
+	/**
+	 * Adds a sub-rule to this composite rule.
+	 *
+	 * @param AbstractRule $rule Sub-rule to be added.
+	 */
+	public function addRule( AbstractRule $rule ) {
+		$this->rules[] = $rule;
+
+		return true;
+	}
+
 }
