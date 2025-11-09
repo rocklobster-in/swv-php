17d6965ac67e0227fb553b5440eb2f73ea4a8e12
Takayuki Miyoshi <takayukister@gmail.com>
2025-11-09 15:45:20 +0900

    Add strip_whitespaces()

--- src/functions.php
+++ src/functions.php
@@ -18,3 +18,34 @@ function array_flatten( mixed $input ): array {
 		return array_merge( $carry, array_flatten( $item ) );
 	}, array() );
 }
+
+
+/**
+ * Strips surrounding whitespaces.
+ *
+ * @param string|array $input Input text or array of text.
+ * @return string|array Output text or array of text.
+ */
+function strip_whitespaces( string|array $input ): string|array {
+	if ( is_array( $input ) ) {
+		return array_map( static function ( $item ) {
+			return strip_whitespaces( $item );
+		}, $input );
+	}
+
+	$whitespaces = '\x09-\x0D\x20\x85\xA0\x{1680}\x{180E}\x{2000}-\x{200A}\x{2028}\x{2029}\x{202F}\x{205F}\x{3000}\x{FEFF}';
+
+	$input = preg_replace(
+		sprintf( '/^[%s]+/u', $whitespaces ),
+		'',
+		$input
+	);
+
+	$input = preg_replace(
+		sprintf( '/[%s]+$/u', $whitespaces ),
+		'',
+		$input
+	);
+
+	return $input;
+}
