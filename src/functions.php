d4e6e36c4a6714c48102178bd835c03285784e3b
Takayuki Miyoshi <takayukister@gmail.com>
2025-11-09 15:08:31 +0900

    Add array_flatten()

--- src/functions.php
+++ src/functions.php
@@ -1,3 +1,20 @@
 <?php
 
 namespace RockLobsterInc\Swv;
+
+
+/**
+ * Converts multi-dimensional array to a flat array.
+ *
+ * @param mixed $input Array or item of array.
+ * @return array Flatten array.
+ */
+function array_flatten( mixed $input ): array {
+	if ( ! is_array( $input ) ) {
+		return array( $input );
+	}
+
+	return array_reduce( $input, static function ( $carry, $item ) {
+		return array_merge( $carry, array_flatten( $item ) );
+	}, array() );
+}
