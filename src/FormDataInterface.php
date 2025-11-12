f5293bdbf117cef95c296a9465d70880d33ec874
Takayuki Miyoshi <takayukister@gmail.com>
2025-11-12 16:56:07 +0900

    Update FormDataInterface.php

--- src/FormDataInterface.php
+++ src/FormDataInterface.php
@@ -12,15 +12,17 @@ interface FormDataInterface {
 	 * Returns the values associated with a given field name.
 	 *
 	 * @param string $name Field name.
+	 * @return array Single dimension array of the values.
 	 */
-	public function get( string $name );
+	public function get( string $name ): array;
 
 
 	/**
 	 * Returns the file objects associated with a given field name.
 	 *
 	 * @param string $name Field name.
+	 * @return array Single dimension array of the values.
 	 */
-	public function getFiles( string $name );
+	public function getFiles( string $name ): array;
 
 }
