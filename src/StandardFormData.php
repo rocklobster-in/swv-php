6a0e995e5a0f499692554522bd32af70b654db7c
Takayuki Miyoshi <takayukister@gmail.com>
2025-11-12 18:02:22 +0900

    Add dissolve()

--- src/StandardFormData.php
+++ src/StandardFormData.php
@@ -11,27 +11,41 @@ class StandardFormData implements FormDataInterface {
 	 * Returns the values associated with a given field name.
 	 *
 	 * @param string $name Field name.
-	 * @return array|string The value(s) associated with the field name.
+	 * @return array Single dimension array of the values.
 	 */
-	public function get( string $name ): array|string {
-		if ( isset( $_FILES[ $name ] ) ) {
-			return $_FILES[ $name ];
-		}
-
-		if ( isset( $_POST[ $name ] ) ) {
-			return $_POST[ $name ];
-		}
+	public function get( string $name ): array {
 	}
 
 
 	/**
-	 * Returns true if the object has the value associated with a given field
-	 * name.
+	 * Returns the file objects associated with a given field name.
 	 *
 	 * @param string $name Field name.
+	 * @return array Single dimension array of the StandardFile objects.
 	 */
-	public function has( string $name ): bool {
-		return isset( $_FILES[ $name ] ) || isset( $_POST[ $name ] );
+	public function getFiles( string $name ): array {
+	}
+
+
+	/**
+	 * Returns parts of a given name.
+	 *
+	 * @param string $name Field name.
+	 * @return array Single dimension array of name parts.
+	 */
+	private function dissolve( string $name ): array {
+		$first_bracket = strpos( $name, '[' );
+
+		if ( false === $first_bracket ) {
+			return [ $name ];
+		}
+
+		$core = substr( $name, 0, $first_bracket );
+		$dimensions = substr( $name, $first_bracket );
+
+		preg_match_all( '/\[(.*?)\]/', $dimensions, $matches );
+
+		return [ $core, ...$matches[1] ];
 	}
 
 }
