b9c5f37d76b90f2e12a2c7869b95cd1c699e3eab
Takayuki Miyoshi <takayukister@gmail.com>
2025-11-15 16:57:07 +0900

    Update Invalidity.php

--- src/Invalidity.php
+++ src/Invalidity.php
@@ -5,4 +5,32 @@ namespace RockLobsterInc\Swv;
 /**
  * Exception that represents a validation error.
  */
-class Invalidity extends \Exception {}
+final class Invalidity extends \Exception {
+
+	/**
+	 * The SWV rule who has thrown this error.
+	 */
+	private AbstractRule $rule;
+
+
+	/**
+	 * Constructor.
+	 *
+	 * @param AbstractRule $rule SWV rule.
+	 */
+	public function __construct( AbstractRule $rule ) {
+		$this->rule = $rule;
+		$this->message = $rule->error ?? '';
+	}
+
+
+	/**
+	 * Resets the validation error message.
+	 *
+	 * @param string $message Validation error message.
+	 */
+	public function setMessage( string $message ) {
+		$this->message = $message;
+	}
+
+}
