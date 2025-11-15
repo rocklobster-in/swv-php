dd842f175a030a148e1d97dc129afc4555927aba
Takayuki Miyoshi <takayukister@gmail.com>
2025-11-15 17:45:28 +0900

    Update any.php

--- src/rules/any.php
+++ src/rules/any.php
@@ -4,11 +4,29 @@ namespace RockLobsterInc\Swv\Rules;
 
 use RockLobsterInc\Swv\{ CompositeRule, Invalidity };
 
-class AnyRule extends CompositeRule {
+final class AnyRule extends CompositeRule {
 
 	const RULE_NAME = 'any';
 
-	public function matches( $context ) {
+	public string $error;
+
+
+	/**
+	 * Constructor.
+	 *
+	 * @param iterable $properties Rule properties.
+	 */
+	public function __construct( iterable $properties = [] ) {
+		$this->error = $properties[ 'error' ] ?? '';
+	}
+
+
+	/**
+	 * Returns true if this rule matches the given context.
+	 *
+	 * @param iterable $context Context.
+	 */
+	public function matches( iterable $context ): bool {
 		if ( false === parent::matches( $context ) ) {
 			return false;
 		}
@@ -16,18 +34,31 @@ class AnyRule extends CompositeRule {
 		return true;
 	}
 
-	public function validate( $context ) {
+
+	/**
+	 * Validates the form data according to the logic defined by this rule.
+	 *
+	 * @param FormDataInterface $form_data Form data.
+	 * @param iterable $context Context.
+	 */
+	public function validate( FormDataInterface $form_data, iterable $context ) {
+		$any_valid = false;
+
 		foreach ( $this->rules() as $rule ) {
 			if ( $rule->matches( $context ) ) {
-				$result = $rule->validate( $context );
-
-				if ( ! is_wp_error( $result ) ) {
-					return true;
+				try {
+					$any_valid = $rule->validate( $form_data, $context );
+				} catch ( Invalidity $error ) {
+					// Do nothing.
 				}
 			}
 		}
 
-		return $this->create_error();
+		if ( ! $any_valid ) {
+			throw new Invalidity( $this );
+		}
+
+		return true;
 	}
 
 }
