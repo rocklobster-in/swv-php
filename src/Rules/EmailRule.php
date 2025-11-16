896f6dca721e690a60d4db2a2a87a192f913eb5a
Takayuki Miyoshi <takayukister@gmail.com>
2025-11-16 14:00:30 +0900

    Update EmailRule.php

--- src/Rules/EmailRule.php
+++ src/Rules/EmailRule.php
@@ -3,32 +3,110 @@
 namespace RockLobsterInc\Swv\Rules;
 
 use RockLobsterInc\Swv\{ AbstractRule, Invalidity };
+use function RockLobsterInc\Swv\{ strip_whitespaces, exclude_blank };
 
 final class EmailRule extends AbstractRule {
 
 	const RULE_NAME = 'email';
 
-	public function matches( $context ) {
+	public string $field;
+	public string $error;
+
+
+	/**
+	 * Constructor.
+	 *
+	 * @param iterable $properties Rule properties.
+	 */
+	public function __construct( iterable $properties = [] ) {
+		$this->field = $properties[ 'field' ] ?? '';
+		$this->error = $properties[ 'error' ] ?? '';
+	}
+
+
+	/**
+	 * Returns true if the given string is a valid email address. This method
+	 * follows WordPress's is_email() implementation.
+	 *
+	 * @link https://developer.wordpress.org/reference/functions/is_email/
+	 *
+	 * @param string $value String to check.
+	 */
+	public static function isEmail( string $value ): bool {
+		if ( strlen( $value ) < 6 ) {
+			return false;
+		}
+
+		if ( false === strpos( $value, '@', 1 ) ) {
+			return false;
+		}
+
+		list( $local, $domain ) = explode( '@', $value, 2 );
+
+		if ( ! preg_match( '/^[a-zA-Z0-9!#$%&\'*+\/=?^_`{|}~\.-]+$/', $local ) ) {
+			return false;
+		}
+
+		if ( preg_match( '/\.{2,}/', $domain ) ) {
+			return false;
+		}
+
+		if ( trim( $domain, " \t\n\r\0\x0B." ) !== $domain ) {
+			return false;
+		}
+
+		$subs = explode( '.', $domain );
+
+		if ( count( $subs ) < 2 ) {
+			return false;
+		}
+
+		foreach ( $subs as $sub ) {
+			if ( trim( $sub, " \t\n\r\0\x0B-" ) !== $sub ) {
+				return false;
+			}
+
+			if ( ! preg_match( '/^[a-z0-9-]+$/i', $sub ) ) {
+				return false;
+			}
+		}
+
+		return true;
+	}
+
+
+	/**
+	 * Returns true if this rule matches the given context.
+	 *
+	 * @param iterable $context Context.
+	 */
+	public function matches( iterable $context ): bool {
 		if ( false === parent::matches( $context ) ) {
 			return false;
 		}
 
-		if ( empty( $context['text'] ) ) {
+		if ( empty( $context[ 'text' ] ) ) {
 			return false;
 		}
 
 		return true;
 	}
 
-	public function validate( $context ) {
-		$input = $this->get_default_input();
-		$input = wpcf7_array_flatten( $input );
-		$input = wpcf7_strip_whitespaces( $input );
-		$input = wpcf7_exclude_blank( $input );
 
-		foreach ( $input as $i ) {
-			if ( ! wpcf7_is_email( $i ) ) {
-				return $this->create_error();
+	/**
+	 * Validates the form data according to the logic defined by this rule.
+	 *
+	 * @param FormDataInterface $form_data Form data.
+	 * @param iterable $context Context.
+	 */
+	public function validate( FormDataInterface $form_data, iterable $context ) {
+		$values = $form_data->getAll( $this->field );
+		$values = strip_whitespaces( $values );
+		$values = exclude_blank( $values );
+
+		foreach ( $values as $value ) {
+			if ( ! self::isEmail( $value ) ) {
+				throw new Invalidity( $this );
 			}
 		}
 
