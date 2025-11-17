53c54b2db7e729a9b81c6d891430d8cb64744f45
Takayuki Miyoshi <takayukister@gmail.com>
2025-11-17 12:02:05 +0900

    MIME_TYPES

--- src/Rules/FileRule.php
+++ src/Rules/FileRule.php
@@ -8,6 +8,204 @@ final class FileRule extends AbstractRule {
 
 	const RULE_NAME = 'file';
 
+	const MIME_TYPES = [
+		'application/java'
+			=> [ 'class' ],
+		'application/javascript'
+			=> [ 'js' ],
+		'application/msword'
+			=> [ 'doc' ],
+		'application/octet-stream'
+			=> [ 'psd', 'xcf' ],
+		'application/onenote'
+			=> [ 'onetoc', 'onetoc2', 'onetmp', 'onepkg' ],
+		'application/oxps'
+			=> [ 'oxps' ],
+		'application/pdf'
+			=> [ 'pdf' ],
+		'application/rar'
+			=> [ 'rar' ],
+		'application/rtf'
+			=> [ 'rtf' ],
+		'application/ttaf+xml'
+			=> [ 'dfxp' ],
+		'application/vnd.apple.keynote'
+			=> [ 'key' ],
+		'application/vnd.apple.numbers'
+			=> [ 'numbers' ],
+		'application/vnd.apple.pages'
+			=> [ 'pages' ],
+		'application/vnd.ms-access'
+			=> [ 'mdb' ],
+		'application/vnd.ms-excel'
+			=> [ 'xla', 'xls', 'xlt', 'xlw' ],
+		'application/vnd.ms-excel.addin.macroEnabled.12'
+			=> [ 'xlam' ],
+		'application/vnd.ms-excel.sheet.binary.macroEnabled.12'
+			=> [ 'xlsb' ],
+		'application/vnd.ms-excel.sheet.macroEnabled.12'
+			=> [ 'xlsm' ],
+		'application/vnd.ms-excel.template.macroEnabled.12'
+			=> [ 'xltm' ],
+		'application/vnd.ms-powerpoint'
+			=> [ 'pot', 'pps', 'ppt' ],
+		'application/vnd.ms-powerpoint.addin.macroEnabled.12'
+			=> [ 'ppam' ],
+		'application/vnd.ms-powerpoint.presentation.macroEnabled.12'
+			=> [ 'pptm' ],
+		'application/vnd.ms-powerpoint.slide.macroEnabled.12'
+			=> [ 'sldm' ],
+		'application/vnd.ms-powerpoint.slideshow.macroEnabled.12'
+			=> [ 'ppsm' ],
+		'application/vnd.ms-powerpoint.template.macroEnabled.12'
+			=> [ 'potm' ],
+		'application/vnd.ms-project'
+			=> [ 'mpp' ],
+		'application/vnd.ms-word.document.macroEnabled.12'
+			=> [ 'docm' ],
+		'application/vnd.ms-word.template.macroEnabled.12'
+			=> [ 'dotm' ],
+		'application/vnd.ms-write'
+			=> [ 'wri' ],
+		'application/vnd.ms-xpsdocument'
+			=> [ 'xps' ],
+		'application/vnd.oasis.opendocument.chart'
+			=> [ 'odc' ],
+		'application/vnd.oasis.opendocument.database'
+			=> [ 'odb' ],
+		'application/vnd.oasis.opendocument.formula'
+			=> [ 'odf' ],
+		'application/vnd.oasis.opendocument.graphics'
+			=> [ 'odg' ],
+		'application/vnd.oasis.opendocument.presentation'
+			=> [ 'odp' ],
+		'application/vnd.oasis.opendocument.spreadsheet'
+			=> [ 'ods' ],
+		'application/vnd.oasis.opendocument.text'
+			=> [ 'odt' ],
+		'application/vnd.openxmlformats-officedocument.presentationml.presentation'
+			=> [ 'pptx' ],
+		'application/vnd.openxmlformats-officedocument.presentationml.slide'
+			=> [ 'sldx' ],
+		'application/vnd.openxmlformats-officedocument.presentationml.slideshow'
+			=> [ 'ppsx' ],
+		'application/vnd.openxmlformats-officedocument.presentationml.template'
+			=> [ 'potx' ],
+		'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'
+			=> [ 'xlsx' ],
+		'application/vnd.openxmlformats-officedocument.spreadsheetml.template'
+			=> [ 'xltx' ],
+		'application/vnd.openxmlformats-officedocument.wordprocessingml.document'
+			=> [ 'docx' ],
+		'application/vnd.openxmlformats-officedocument.wordprocessingml.template'
+			=> [ 'dotx' ],
+		'application/wordperfect'
+			=> [ 'wp', 'wpd' ],
+		'application/x-7z-compressed'
+			=> [ '7z' ],
+		'application/x-gzip'
+			=> [ 'gz', 'gzip' ],
+		'application/x-msdownload'
+			=> [ 'exe' ],
+		'application/x-shockwave-flash'
+			=> [ 'swf' ],
+		'application/x-tar'
+			=> [ 'tar' ],
+		'application/zip'
+			=> [ 'zip' ],
+		'audio/aac'
+			=> [ 'aac' ],
+		'audio/flac'
+			=> [ 'flac' ],
+		'audio/midi'
+			=> [ 'mid', 'midi' ],
+		'audio/mpeg'
+			=> [ 'mp3', 'm4a', 'm4b' ],
+		'audio/ogg'
+			=> [ 'ogg', 'oga' ],
+		'audio/wav'
+			=> [ 'wav', 'x-wav' ],
+		'audio/x-matroska'
+			=> [ 'mka' ],
+		'audio/x-ms-wax'
+			=> [ 'wax' ],
+		'audio/x-ms-wma'
+			=> [ 'wma' ],
+		'audio/x-realaudio'
+			=> [ 'ra', 'ram' ],
+		'image/avif'
+			=> [ 'avif' ],
+		'image/bmp'
+			=> [ 'bmp' ],
+		'image/gif'
+			=> [ 'gif' ],
+		'image/heic'
+			=> [ 'heic' ],
+		'image/heic-sequence'
+			=> [ 'heics' ],
+		'image/heif'
+			=> [ 'heif' ],
+		'image/heif-sequence'
+			=> [ 'heifs' ],
+		'image/jpeg'
+			=> [ 'jpg', 'jpeg', 'jpe' ],
+		'image/png'
+			=> [ 'png' ],
+		'image/tiff'
+			=> [ 'tiff', 'tif' ],
+		'image/webp'
+			=> [ 'webp' ],
+		'image/x-icon'
+			=> [ 'ico' ],
+		'text/calendar'
+			=> [ 'ics' ],
+		'text/css'
+			=> [ 'css' ],
+		'text/csv'
+			=> [ 'csv' ],
+		'text/html'
+			=> [ 'htm', 'html' ],
+		'text/plain'
+			=> [ 'txt', 'asc', 'c', 'cc', 'h', 'srt' ],
+		'text/richtext'
+			=> [ 'rtx' ],
+		'text/tab-separated-values'
+			=> [ 'tsv' ],
+		'text/vtt'
+			=> [ 'vtt' ],
+		'video/3gpp'
+			=> [ '3gp', '3gpp' ],
+		'video/3gpp2'
+			=> [ '3g2', '3gp2' ],
+		'video/avi'
+			=> [ 'avi' ],
+		'video/divx'
+			=> [ 'divx' ],
+		'video/mp4'
+			=> [ 'mp4', 'm4v' ],
+		'video/mpeg'
+			=> [ 'mpeg', 'mpg', 'mpe' ],
+		'video/ogg'
+			=> [ 'ogv' ],
+		'video/quicktime'
+			=> [ 'mov', 'qt' ],
+		'video/webm'
+			=> [ 'webm' ],
+		'video/x-flv'
+			=> [ 'flv' ],
+		'video/x-matroska'
+			=> [ 'mkv' ],
+		'video/x-ms-asf'
+			=> [ 'asf', 'asx' ],
+		'video/x-ms-wm'
+			=> [ 'wm' ],
+		'video/x-ms-wmv'
+			=> [ 'wmv' ],
+		'video/x-ms-wmx'
+			=> [ 'wmx' ],
+	];
+
+
 	public string $field;
 	public string $error;
 	public array $accept;
