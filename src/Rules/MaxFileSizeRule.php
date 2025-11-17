30f58c00186a750e63b40ca54aaf1d649e044102
Takayuki Miyoshi <takayukister@gmail.com>
2025-11-17 14:00:52 +0900

    Update MaxFileSizeRule.php

--- src/Rules/MaxFileSizeRule.php
+++ src/Rules/MaxFileSizeRule.php
@@ -8,31 +8,61 @@ final class MaxFileSizeRule extends AbstractRule {
 
 	const RULE_NAME = 'maxfilesize';
 
-	public function matches( $context ) {
+
+	/**
+	 * Rule properties.
+	 */
+	public string $field;
+	public string $error;
+	public string $threshold;
+
+
+	/**
+	 * Constructor.
+	 *
+	 * @param iterable $properties Rule properties.
+	 */
+	public function __construct( iterable $properties = [] ) {
+		$this->field = $properties[ 'field' ] ?? '';
+		$this->error = $properties[ 'error' ] ?? '';
+		$this->threshold = $properties[ 'threshold' ] ?? '';
+	}
+
+
+	/**
+	 * Returns true if this rule matches the given context.
+	 *
+	 * @param iterable $context Context.
+	 */
+	public function matches( iterable $context ): bool {
 		if ( false === parent::matches( $context ) ) {
 			return false;
 		}
 
-		if ( empty( $context['file'] ) ) {
+		if ( empty( $context[ 'file' ] ) ) {
 			return false;
 		}
 
 		return true;
 	}
 
-	public function validate( $context ) {
-		$input = $this->get_default_upload()->size ?? '';
-		$input = wpcf7_array_flatten( $input );
-		$input = wpcf7_exclude_blank( $input );
 
-		if ( empty( $input ) ) {
-			return true;
-		}
+	/**
+	 * Validates the form data according to the logic defined by this rule.
+	 *
+	 * @param FormDataInterface $form_data Form data.
+	 * @param iterable $context Context.
+	 */
+	public function validate( FormDataInterface $form_data, iterable $context ) {
+		$files = $form_data->getAllFiles( $this->field );
 
-		$threshold = $this->get_property( 'threshold' );
+		$file_size = array_reduce( $files, static function ( $carry, $item ) {
+			$carry += $item->size();
+			return $carry;
+		}, 0 );
 
-		if ( $threshold < array_sum( $input ) ) {
-			return $this->create_error();
+		if ( (int) $this->threshold < $file_size ) {
+			throw new Invalidity( $this );
 		}
 
 		return true;
