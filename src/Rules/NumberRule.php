2efa7ab4b0e2a1a59249f8d2102b1c031937c2b8
Takayuki Miyoshi <takayukister@gmail.com>
2025-11-17 18:28:02 +0900

    Update NumberRule.php

--- src/Rules/NumberRule.php
+++ src/Rules/NumberRule.php
@@ -3,32 +3,86 @@
 namespace RockLobsterInc\Swv\Rules;
 
 use RockLobsterInc\Swv\{ AbstractRule, Invalidity };
+use function RockLobsterInc\Swv\{ strip_whitespaces, exclude_blank };
 
 final class NumberRule extends AbstractRule {
 
 	const RULE_NAME = 'number';
 
-	public function matches( $context ) {
+
+	/**
+	 * Rule properties.
+	 */
+	public string $field;
+	public string $error;
+
+
+	/**
+	 * Constructor.
+	 *
+	 * @param iterable $properties Rule properties.
+	 */
+	public function __construct( iterable $properties = [] ) {
+		$this->field = $properties[ 'field' ] ?? '';
+		$this->error = $properties[ 'error' ] ?? '';
+	}
+
+
+	/**
+	 * Returns true if the given string is a well-formed number.
+	 *
+	 * @link https://html.spec.whatwg.org/multipage/input.html#number-state-(type=number)
+	 *
+	 * @param string $value String to check.
+	 */
+	public static function isNumber( string $value ): bool {
+		$patterns = [
+			'/^[-]?[0-9]+(?:[eE][+-]?[0-9]+)?$/',
+			'/^[-]?(?:[0-9]+)?[.][0-9]+(?:[eE][+-]?[0-9]+)?$/',
+		];
+
+		foreach ( $patterns as $pattern ) {
+			if ( preg_match( $pattern, $value ) ) {
+				return true;
+			}
+		}
+
+		return false;
+	}
+
+
+	/**
+	 * Returns true if this rule matches the given context.
+	 *
+	 * @param iterable $context Context.
+	 */
+	public function matches( iterable $context ): bool {
 		if ( false === parent::matches( $context ) ) {
 			return false;
 		}
 
-		if ( empty( $context['text'] ) ) {
+		if ( empty( $context[ 'text' ] ) ) {
 			return false;
 		}
 
 		return true;
 	}
 
-	public function validate( $context ) {
-		$input = $this->get_default_input();
-		$input = wpcf7_array_flatten( $input );
-		$input = wpcf7_strip_whitespaces( $input );
-		$input = wpcf7_exclude_blank( $input );
 
-		foreach ( $input as $i ) {
-			if ( ! wpcf7_is_number( $i ) ) {
-				return $this->create_error();
+	/**
+	 * Validates the form data according to the logic defined by this rule.
+	 *
+	 * @param FormDataInterface $form_data Form data.
+	 * @param iterable $context Context.
+	 */
+	public function validate( FormDataInterface $form_data, iterable $context ) {
+		$values = $form_data->getAll( $this->field );
+		$values = strip_whitespaces( $values );
+		$values = exclude_blank( $values );
+
+		foreach ( $values as $value ) {
+			if ( ! self::isNumber( $value ) ) {
+				throw new Invalidity( $this );
 			}
 		}
 
