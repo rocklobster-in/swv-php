2af4adaad7cde6deedbffd883e677e9370aede37
Takayuki Miyoshi <takayukister@gmail.com>
2025-11-18 16:22:27 +0900

    Update MinNumberRule.php

--- src/Rules/MinNumberRule.php
+++ src/Rules/MinNumberRule.php
@@ -3,38 +3,72 @@
 namespace RockLobsterInc\Swv\Rules;
 
 use RockLobsterInc\Swv\{ AbstractRule, Invalidity };
+use function RockLobsterInc\Swv\{ strip_whitespaces, exclude_blank };
 
 final class MinNumberRule extends AbstractRule {
 
 	const RULE_NAME = 'minnumber';
 
-	public function matches( $context ) {
+
+	/**
+	 * Rule properties.
+	 */
+	public string $field;
+	public string $error;
+	public string $threshold;
+
+
+	/**
+	 * Constructor.
+	 *
+	 * @param iterable $properties Rule properties.
+	 */
+	public function __construct( iterable $properties = [] ) {
+		$this->field = $properties[ 'field' ] ?? '';
+		$this->error = $properties[ 'error' ] ?? '';
+		$this->threshold = $properties[ 'threshold' ] ?? '';
+	}
+
+
+	/**
+	 * Returns true if this rule matches the given context.
+	 *
+	 * @param iterable $context Context.
+	 */
+	public function matches( iterable $context ): bool {
 		if ( false === parent::matches( $context ) ) {
 			return false;
 		}
 
-		if ( empty( $context['text'] ) ) {
+		if ( empty( $context[ 'text' ] ) ) {
 			return false;
 		}
 
 		return true;
 	}
 
-	public function validate( $context ) {
-		$input = $this->get_default_input();
-		$input = wpcf7_array_flatten( $input );
-		$input = wpcf7_strip_whitespaces( $input );
-		$input = wpcf7_exclude_blank( $input );
 
-		$threshold = $this->get_property( 'threshold' );
+	/**
+	 * Validates the form data according to the logic defined by this rule.
+	 *
+	 * @param FormDataInterface $form_data Form data.
+	 * @param iterable $context Context.
+	 */
+	public function validate( FormDataInterface $form_data, iterable $context ) {
+		$values = $form_data->getAll( $this->field );
+		$values = strip_whitespaces( $values );
+		$values = exclude_blank( $values );
 
-		if ( ! wpcf7_is_number( $threshold ) ) {
+		if ( ! is_numeric( $this->threshold ) ) {
 			return true;
 		}
 
-		foreach ( $input as $i ) {
-			if ( wpcf7_is_number( $i ) and (float) $i < (float) $threshold ) {
-				return $this->create_error();
+		foreach ( $values as $value ) {
+			if (
+				NumberRule::isNumber( $value ) and
+				(float) $value < (float) $this->threshold
+			) {
+				throw new Invalidity( $this );
 			}
 		}
 
